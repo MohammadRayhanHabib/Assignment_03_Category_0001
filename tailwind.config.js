6f8c0a1fe3747726cf1bca7a06d9b7f53fc6f5ef
MohammadRayhanHabib <177920153+MohammadRayhanHabib@users.noreply.github.com>
2025-02-01 08:47:43 +0600

    btn-animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,30 @@
-// module.exports = {
-//   content: [
-//     "./src/**/*.{html,js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
-
 module.exports = {
   content: [
     "./src/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      screens: {
-        'xs': '360px',  // Custom breakpoint for small phones
-        'sm': '480px',  // Custom breakpoint for larger mobile devices
-      
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
+
+
+
+/**  */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"], 
+//   theme: {
+//     extend: {
+//       animation: {
+//         'loop-bounce': 'loopBounce 1.5s ease-in-out infinite',
+//       },
+//       keyframes: {
+//         loopBounce: {
+//           '0%, 100%': { transform: 'translateY(0)' },
+//           '50%': { transform: 'translateY(-5px)' },
+//         },
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
